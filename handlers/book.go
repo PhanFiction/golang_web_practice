f56c134cf735632b1efbab2f49246d679943174a
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-04-25 18:52:45 -0700

    Updated template usage and imported to use custom Types

--- handlers/book.go
+++ handlers/book.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"goweb_exercise/types"
 	"html/template"
 	"net/http"
 	"strconv"
@@ -14,7 +15,12 @@ type BookPageData struct {
 }
 
 func BookPageHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("book.html"))
+	tmpl := template.Must(template.New("layout.html").
+		ParseFiles(
+			"templates/layout.html",
+			"templates/book.html",
+		))
+
 	vars := mux.Vars(r)
 	title := vars["title"]
 	page := vars["page"]
@@ -26,21 +32,11 @@ func BookPageHandler(w http.ResponseWriter, r *http.Request) {
 		pageNumber = 1
 	}
 
-	data := BookPageData{
-		PageTitle:  title,
-		PageNumber: pageNumber,
+	data := types.PageData{
+		TabTitle:  "Book Page",
+		PageTitle: title,
+		BookPage:  pageNumber,
 	}
 
-	// fmt.Fprintf(w, `
-	// 	<html>
-	// 		<body>
-	// 			<h1>Welcome to my website!</h1>
-	// 			<p>Title is %s </p>
-	// 			<p>Page is %s</p>
-	// 			<img src="/static/img.jpeg" alt="A cute cat" style="max-width: 300px;" />
-	// 		</body>
-	// 	</html>
-	// `, title, page)
-
-	tmpl.Execute(w, data)
+	tmpl.ExecuteTemplate(w, "layout.html", data)
 }
