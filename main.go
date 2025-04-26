d729dff54279f8012518c1d28eafeb177d020cec
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-04-25 18:48:32 -0700

    Updated main.go  - Seperated routes and serving static files

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"goweb_exercise/routes"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -9,30 +9,6 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-
-	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		title := vars["title"]
-		page := vars["page"]
-		fmt.Fprintf(w, `
-			<html>
-					<body>
-							<h1>Welcome to my website!</h1>
-							<p>Title is %s </p>
-							<p>Page is %s</p>
-							<img src="/static/img.jpeg" alt="A cute cat" style="max-width: 300px;" />
-					</body>
-			</html>
-		`, title, page)
-	})
-
-	// Serving static files using gorilla/mux
-	fs := http.FileServer(http.Dir("static/"))
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
-
-	// Default way to serve static files
-	// fs := http.FileServer(http.Dir("static/"))                // point to where static directory is located at
-	// http.Handle("/static/", http.StripPrefix("/static/", fs)) // Strip away part of the url path and serve the files from here
-
+	routes.SetupRoutes(r)
 	http.ListenAndServe(":8080", r)
 }
