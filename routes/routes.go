f252f6431df5889ef449836d8bfb2441f2e926c3
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-04-25 18:49:03 -0700

    Added route  - Created book route

--- routes/routes.go
+++ routes/routes.go
@@ -10,6 +10,7 @@ import (
 func SetupRoutes(r *mux.Router) {
 	r.HandleFunc("/", handlers.HomeHandler).Methods("GET")
 	r.HandleFunc("/about", handlers.AboutHandler).Methods("GET")
+	r.HandleFunc("/books/{title}/page/{page}", handlers.BookPageHandler)
 
 	fs := http.FileServer(http.Dir("static/"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
