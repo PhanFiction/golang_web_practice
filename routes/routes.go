97dd78a5941717066fee15a71ac1d01af116bd2a
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-04-28 19:25:49 -0700

    Updated routes to use middlewares

--- routes/routes.go
+++ routes/routes.go
@@ -2,16 +2,17 @@ package routes
 
 import (
 	"goweb_exercise/handlers"
+	"goweb_exercise/middleware"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func SetupRoutes(r *mux.Router) {
-	r.HandleFunc("/", handlers.HomeHandler).Methods("GET")
-	r.HandleFunc("/about", handlers.AboutHandler).Methods("GET")
-	r.HandleFunc("/books/{title}/page/{page}", handlers.BookPageHandler).Methods("GET")
-	r.HandleFunc("/create-book", handlers.CreateBookHandler).Methods("POST")
+	r.HandleFunc("/", middleware.Chain(handlers.HomeHandler, middleware.Logging())).Methods("GET")
+	r.HandleFunc("/about", middleware.Chain(handlers.AboutHandler, middleware.Logging())).Methods("GET")
+	r.HandleFunc("/books/{title}/page/{page}", middleware.Chain(handlers.BookPageHandler, middleware.Logging())).Methods("GET")
+	r.HandleFunc("/create-book", middleware.Chain(handlers.CreateBookHandler, middleware.Logging())).Methods("POST")
 
 	fs := http.FileServer(http.Dir("static/"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
