739f9caf3417ad28d04a25c33af3eea22807b6e2
Steve <81824005+PhanFiction@users.noreply.github.com>
2025-04-30 21:13:26 -0700

    Added fetch user data to check if user is authenticated

--- internal/handlers/about.go
+++ internal/handlers/about.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"goweb_exercise/internal/session"
 	"goweb_exercise/internal/types"
 	"html/template"
 	"net/http"
@@ -10,12 +11,17 @@ func AboutHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.New("layout.html").
 		ParseFiles(
 			"templates/layout.html",
+			"templates/nav.html",
 			"templates/about.html",
 		))
 
+	session, _ := session.Store.Get(r, "session")
+	auth, ok := session.Values["authenticated"].(bool)
+
 	data := types.PageData{
-		TabTitle:  "About Page",
-		PageTitle: "About",
+		TabTitle:      "About Page",
+		PageTitle:     "About",
+		Authenticated: ok && auth,
 	}
 
 	tmpl.ExecuteTemplate(w, "layout.html", data)
--- internal/handlers/dashboard.go
+++ internal/handlers/dashboard.go
@@ -7,20 +7,20 @@ import (
 	"net/http"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func DashboardHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.New("layout.html").
 		ParseFiles(
 			"templates/layout.html",
 			"templates/nav.html",
-			"templates/home.html",
+			"templates/dashboard.html",
 		))
 
 	session, _ := session.Store.Get(r, "session")
 	auth, ok := session.Values["authenticated"].(bool)
 
 	data := types.PageData{
-		TabTitle:      "Home Page",
-		PageTitle:     "Home",
+		Username:      "Alex",
+		TabTitle:      "Dashboard",
 		Authenticated: ok && auth,
 	}
 
